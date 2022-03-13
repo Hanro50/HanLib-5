408f37476b759b253594b0d1abd2d489f0031387
Hanro <hlareman2@gmail.com>
2022-03-13 15:31:52 +0200

    Bungeecord support!

--- plugin/src/main/java/za/net/hanro50/util/Bungee.java
+++ plugin/src/main/java/za/net/hanro50/util/Bungee.java
@@ -1,5 +1,31 @@
 package za.net.hanro50.util;
 
-public class Bungee {
-    
+import java.io.File;
+import java.io.IOException;
+
+import net.md_5.bungee.api.plugin.Plugin;
+import za.net.hanro50.util.log.Console;
+import za.net.hanro50.util.log.Settings;
+
+public class Bungee extends Plugin {
+    final Settings settings = (Settings) new Settings(new File(this.getDataFolder(), "HanLib.prop"),
+            getLogger().getParent());
+    @Override
+    public void onEnable() {
+        try {
+            settings.init().save();
+        } catch (IOException e) {
+            Console.crt(
+                    "Hanlib has failed to load its own config file.\nIt cannot continue to operate due to this error!\nPlease check disk permissions before reporting this as a bug.");
+            this.onDisable();
+            return;
+        }
+        Settings.Register("HanLib");
+        Console.out("Hanlib has been loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        Console.out("Hanlib has been unloaded :(");
+    }
 }
